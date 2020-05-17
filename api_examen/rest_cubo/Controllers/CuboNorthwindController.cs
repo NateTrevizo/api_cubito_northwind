4021ba1e21256aeb27baa45a263c680b6bbf9d01
FridaTrevizo <33066660+FridaTrevizo@users.noreply.github.com>
2020-05-17 16:05:11 -0500

    year search

--- api_examen/rest_cubo/Controllers/CuboNorthwindController.cs
+++ api_examen/rest_cubo/Controllers/CuboNorthwindController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.AnalysisServices.AdomdClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -8,25 +12,135 @@ using System.Web.Http.Cors;
 
 namespace rest_cubo.Controllers
 {
-    [EnableCors(origins: "*", headers: "*", methods:"*")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     [RoutePrefix("v1/Analisis/Northwind")]
     public class CuboNorthwindController : ApiController
     {
         [HttpGet]
         [Route("Top5")]
-
         public HttpResponseMessage Top5()
         {
             string dimension = "[Dim Cliente].[Dim Cliente Nombre].CHILDREN";
             string WITH = @"
                 WITH
-                SET [TopVentas] AS NONEMPTY(
+                SET [TopVentas] AS
+                NONEMPTY(
                     ORDER(
+                        STRTOSET(@Dimension),
+                        [Measures].[Fact Ventas Netas], DESC
                     )
                 )
-                "
-            return Request.CreateErrorResponse(HttpStatusCode.OK, "");
+            ";
+            string COLUMNS = @"
+                NON EMPTY
+                {
+                    [Measures].[Fact Ventas Netas]
+                }
+                ON COLUMNS,    
+            ";
+            string ROWS = @"
+                NON EMPTY
+                {
+                    HEAD([TopVentas], 5)
+                }
+                ON ROWS
+            ";
+            string CUBO_NAME = "[DWHNorthwind]";
+            string MDX_QUERY = WITH + @"SELECT " + COLUMNS + ROWS + " FROM " + CUBO_NAME;
+            Debug.Write(MDX_QUERY);
+
+            List<string> clients = new List<string>();
+            List<decimal> ventas = new List<decimal>();
+            List<dynamic> lstTabla = new List<dynamic>();
+
+            dynamic result = new
+            {
+                datoDimension = clients,
+                datosVenta = ventas,
+                datosTabla = lstTabla
+            };
+
+            using (AdomdConnection cnn = new AdomdConnection(ConfigurationManager.ConnectionStrings["CuboNorthwind"].ConnectionString))
+            {
+                cnn.Open();
+                using (AdomdCommand cmd = new AdomdCommand(MDX_QUERY, cnn))
+                {
+                    cmd.Parameters.Add("Dimension", dimension);
+                    using (AdomdDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                    {
+                        while (dr.Read())
+                        {
+                            clients.Add(dr.GetString(0));
+                            ventas.Add(Math.Round(dr.GetDecimal(1)));
+
+                            dynamic objTabla = new
+                            {
+                                descripcion = dr.GetString(0),
+                                valor = Math.Round(dr.GetDecimal(1))
+                            };
+                            lstTabla.Add(objTabla);
+                        }
+                        dr.Close();
+                    }
+                }
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, (object)result);
         }
 
+        [HttpGet]
+        [Route("AnosVentas")]
+        public HttpResponseMessage AnosVentas()
+        {
+            string dimension = "[Dim Tiempo].[Dim Tiempo Año].Children";
+
+            string COLUMNS = @"
+                NON EMPTY
+                {
+                    [Measures].[Fact Ventas Netas]
+                }
+                ON COLUMNS,    
+            ";
+            string ROWS = @"
+                NON EMPTY
+                {
+                    [Dim Tiempo].[Dim Tiempo Año].Children
+                }
+                ON ROWS
+            ";
+            string CUBO_NAME = "[DWHNorthwind]";
+            string MDX_QUERY = @"SELECT " + COLUMNS + ROWS + " FROM " + CUBO_NAME;
+            Debug.Write(MDX_QUERY);
+
+            List<string> anos = new List<string>();
+            List<decimal> ventas = new List<decimal>();
+            List<dynamic> lstTabla = new List<dynamic>();
+
+            dynamic result = new
+            {
+                datoDimension = anos,
+                datosVenta = ventas
+            };
+
+            using (AdomdConnection cnn = new AdomdConnection(ConfigurationManager.ConnectionStrings["CuboNorthwind"].ConnectionString))
+            {
+                cnn.Open();
+                using (AdomdCommand cmd = new AdomdCommand(MDX_QUERY, cnn))
+                {
+                    cmd.Parameters.Add("Dimension", dimension);
+                    using (AdomdDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                    {
+                        while (dr.Read())
+                        {
+                            anos.Add(dr.GetString(0));
+                            ventas.Add(Math.Round(dr.GetDecimal(1)));
+                        }
+                        dr.Close();
+                    }
+                }
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, (object)result);
+        }
     }
 }
