c1df46f646fdca45b452b5b50a99f5ca019d4c34
FridaTrevizo <33066660+FridaTrevizo@users.noreply.github.com>
2020-05-17 14:59:13 -0500

    half of the stuff

---
+++ api_examen/rest_cubo/Controllers/CuboNorthwindController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace rest_cubo.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods:"*")]
+    [RoutePrefix("v1/Analisis/Northwind")]
+    public class CuboNorthwindController : ApiController
+    {
+        [HttpGet]
+        [Route("Top5")]
+
+        public HttpResponseMessage Top5()
+        {
+            string dimension = "[Dim Cliente].[Dim Cliente Nombre].CHILDREN";
+            string WITH = @"
+                WITH
+                SET [TopVentas] AS NONEMPTY(
+                    ORDER(
+                    )
+                )
+                "
+            return Request.CreateErrorResponse(HttpStatusCode.OK, "");
+        }
+
+    }
+}
